bec6f3ac20725f5d7ed721ba173e2bdceb7340c6
jramirezc93 <jorgerc93@msn.com>
2014-07-16 13:22:45 +0200

    Server

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 
 //!/usr/bin/nodejs
+var d3 = require("d3");
+var fs = require('fs');
+var csvrow = require('csv-string');
 var WebSocketServer = require('ws').Server
   , wss = new WebSocketServer({port: 8080});
 
@@ -11,59 +14,23 @@ wss.on('connection', function(ws) {
     });
     ws.send('');
     ws.send(' cpu1 int1 mem1 cpu2 int2 mem2 cpu3 int3 mem3 cpu4 int4 mem4');
-    setTimeout(function() {
-        ws.send(Date.now().toString(), {mask: true});
-    }, 5000);
-    ws.send(' 2 0 0 1488 144 5916 2 2 57 75 142 42');
-    setTimeout(function() {
-        ws.send(Date.now().toString(), {mask: true});
-    }, 5000);
-    ws.send(' 2 1 2 10098488 1344 3915916 0 2 52 75 142 492');
-    setTimeout(function() {
-        ws.send(Date.now().toString(), {mask: true});
-    }, 5000);
-    ws.send(' 2 5 3 20098488 3344 3315916 0 0 57 35 342 492');
-    setTimeout(function() {
-        ws.send(Date.now().toString(), {mask: true});
-    }, 5000);
-    ws.send(' 2 4 0 14098488 1344 3125916 1 0 57 75 142 492');
-    setTimeout(function() {
-        ws.send(Date.now().toString(), {mask: true});
-    }, 5000);
-    ws.send(' 2 1 2 10098488 1344 3915916 0 2 52 75 142 492');
-    setTimeout(function() {
-        ws.send(Date.now().toString(), {mask: true});
-    }, 5000);
-    ws.send(' 2 5 3 20098488 3344 3315916 0 0 57 35 342 492');
-    setTimeout(function() {
-        ws.send(Date.now().toString(), {mask: true});
-    }, 5000);
-    ws.send(' 2 4 0 14098488 1344 3125916 1 0 57 75 142 492');
-    setTimeout(function() {
-        ws.send(Date.now().toString(), {mask: true});
-    }, 500);
-    ws.send(' 2 0 0 10098488 1344 3115916 0 0 57 75 142 492');
-    setTimeout(function() {
-        ws.send(Date.now().toString(), {mask: true});
-    }, 5000);
-    ws.send(' 2 1 2 10098488 1344 3915916 0 2 52 75 142 492');
-    setTimeout(function() {
-        ws.send(Date.now().toString(), {mask: true});
-    }, 5000);
-    ws.send(' 2 1 2 10098488 1344 3915916 43430 222 52 75 142 492');
-    setTimeout(function() {
-        ws.send(Date.now().toString(), {mask: true});
-    }, 5000);
-    ws.send(' 2 5 3 20098488 3344 3315916 3330 3330 57 35 342 492');
-    setTimeout(function() {
-        ws.send(Date.now().toString(), {mask: true});
-    }, 5000);
-    ws.send(' 2 4 0 14098488 1344 3125916 1 0 57 75 142 492');
-    setTimeout(function() {
-        ws.send(Date.now().toString(), {mask: true});
-    }, 5000);
-    ws.send(' 2 5 3 20098488 3344 3315916 0 0 57 35 342 492');
+    var ruta = "public/csv/nodes.csv";
+    var i =0;
+    fs.readFile(ruta, 'utf8', function (err, data) {
+        d3.csv.parse(data);
+        csvrow.forEach(data, '/b', function(row, index) {
+            for(i = 0;i<index;i++)
+            {
+                ws.send(''+row);
+                setTimeout(function () {
+                    ws.send(Date.now().toString(), {mask: true});
+                }, 5000);
+            }
 
+
+        });
+
+    })
 
 
 
